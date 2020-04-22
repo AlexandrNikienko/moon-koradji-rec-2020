f80ef2cf47dbb618e00c4fb7d21288389f5f64d4
anikienko <anikienko.callfire.com>
2020-04-22 16:01:36 +0300

    refactoring

--- src/app/releases/releases.component.ts
+++ src/app/releases/releases.component.ts
@@ -3,8 +3,7 @@ import { DiscographyService } from '../core/services/discography.service';
 
 @Component({
   selector: 'app-releases',
-  templateUrl: './releases.component.html',
-  styleUrls: ['./releases.component.css']
+  templateUrl: './releases.component.html'
 })
 export class ReleasesComponent implements OnInit {
   public releases$ = this.releasesService.RELEASES$;
