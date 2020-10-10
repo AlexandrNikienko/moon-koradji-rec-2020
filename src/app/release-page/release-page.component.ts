99b0b3dc4bd00b5857b0bc2551f097199b03789a
anikienko <anikienko.callfire.com>
2020-10-10 23:00:22 +0300

    switchMap optimization

--- src/app/release-page/release-page.component.ts
+++ src/app/release-page/release-page.component.ts
@@ -1,28 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { forkJoin, Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil, switchMap } from 'rxjs/operators';
 import { Meta, Title } from '@angular/platform-browser';
 
 import { Artist } from '../core/models/artist.model';
 import { Release } from '../core/models/release.model';
 import { DataService } from '../core/services/data.service';
 
-import { IMAGEFOLDER } from '../../environments/environment';
-
 @Component({
 	selector: 'app-release-page',
 	templateUrl: './release-page.component.html'
 })
+
 export class ReleasePageComponent implements OnInit {
-	public coverFolder = IMAGEFOLDER + 'release-cover/';
-	public releases$ = this.dataService.requestToData('releases');
-	public artists$ = this.dataService.requestToData('artists');
-	public releaseRoute: string;
-	public release: Release;
-	public releaseArtists: Array<string>;
-	public involved: Artist[] = [];
-	private releasesSub: Subscription;
-	private joinSub: Subscription;
+	release: Release;
+	involved: Artist[] = [];
+	private releases$ = this.dataService.requestToData('releases');
+	private artists$ = this.dataService.requestToData('artists');
+	private releaseRoute = this.route.snapshot.params['releaseRoute'];
+	private destroyStream = new Subject<void>();
 
 	constructor(private route: ActivatedRoute,
 		private dataService: DataService,
@@ -31,40 +28,29 @@ export class ReleasePageComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.releaseRoute = this.route.snapshot.params['releaseRoute'];
-
-		this.releases$
-			.subscribe(releases => {
-				this.release = releases.filter(obj => obj['releaseRoute'] === this.releaseRoute)[0];
-				this.releaseArtists = this.release.artists;
-				//this.setMetaData(this.release);
-				this.title.setTitle(this.release.releaseTitle);
-			});
-
-		forkJoin(this.artists$, this.releases$)
-			.subscribe(
-				data => {
-					const allArtistsList = data[0];
-					this.createInvolvedArtistsArray(allArtistsList, this.releaseArtists);
-				}
-			);
+		this.releases$.pipe(
+			switchMap(releases => {
+				this.release = releases.find((release: Release) => release['releaseRoute'] === this.releaseRoute);
+				return this.artists$;
+			}),
+			takeUntil(this.destroyStream)
+		).subscribe(artists => {
+			this.title.setTitle(this.release.releaseTitle);
+			this.involved = this.getInvolvedArtists(artists, this.release.artists);
+		})
 	}
 
-	ngOnDestroy(): void {
-		if (this.releasesSub) {
-			this.releasesSub.unsubscribe();
-		}
-		if (this.joinSub) {
-			this.joinSub.unsubscribe();
-		}
+	ngOnDestroy() {
+		this.destroyStream.next();
 	}
 
-	createInvolvedArtistsArray(allArtists: Artist[], releaseArtists: string[]): void {
-		const self = this;
-		releaseArtists.forEach((name) => {
-			const artist = allArtists.filter(arr => arr.artistName === name);
-			self.involved.push(artist[0]);
+	getInvolvedArtists(allArtists: Artist[], releaseArtists: string[]): Artist[] {
+		let involved = [];
+		releaseArtists.forEach(name => {
+			const artist = allArtists.find(artist => artist.artistName === name);
+			involved.push(artist);
 		});
+		return involved;
 	}
 
 	setMetaData(release: Release): void {
