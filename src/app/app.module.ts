abf73c67b8b251567b6122cc1872ec65c92ef941
anikienko <anikienko.callfire.com>
2022-05-07 18:36:01 +0300

    add support ua banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,3 @@
-
-
-
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,7 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
 })
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit { 
+	showSupportUkraine: boolean;
+	localStorage: string | null;
+	expiredIn = 30; // days
+	showAgain: boolean;
+
+	ngOnInit(): void {
+		const now = new Date().getTime();
+		this.localStorage = localStorage.getItem('supportUkraine');
+		this.showAgain = this.localStorage &&
+							now - JSON.parse(this.localStorage).hiddenTime > this.expiredIn*24*60*60*1000;
+
+		if (this.showAgain) localStorage.clear();
+
+		this.showSupportUkraine =  !this.localStorage;
+	}
+	
+	closeSupportBanner(): void {
+		this.showSupportUkraine = false;
+		localStorage.setItem('supportUkraine', JSON.stringify({
+			hiddenTime: new Date().getTime()
+		}));
+	}
+}
