c433be3c32f5271d1da47493b0cf8aa26dfe7be7
Oleksandr Nikiienko <oleksandr.nikiienko@innervate.com>
2026-02-12 08:21:33 +0200

    nexus released

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -66,7 +66,7 @@ export class HomeComponent {
 			
 			const today = new Date();
 			today.setHours(0, 0, 0, 0);
-			releaseDate.setHours(12, 0, 0, 0);
+			releaseDate.setHours(12, 0, 0, 0); //12AM
 			
 			return releaseDate > today ? 'Coming Soon' : 'Out Now';
 		} catch {
